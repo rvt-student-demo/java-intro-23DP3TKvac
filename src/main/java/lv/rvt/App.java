fca5cc90a0adac1d2cdf752e5968d83551d8e63c
Timurs Kvačovs <166509350+23DP3TKvac@users.noreply.github.com>
2024-10-31 12:40:51 +0000

    Array Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,7 +4,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
-        System.out.println("rvt-student");
+        int[] val = {0, 1, 2, 3}; 
+
+        
+        int sum = val[0] + val[1] + val[2] + val[3];
+     
+        System.out.println( "Sum of all numbers = " + sum );
     }
 }
