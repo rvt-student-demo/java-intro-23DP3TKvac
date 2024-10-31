a1785644176336ffbe6d4e74089ee5577625dbe5
Timurs Kvačovs <166509350+23DP3TKvac@users.noreply.github.com>
2024-10-31 12:49:47 +0000

    Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,11 +4,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] val = {0, 1, 2, 3}; 
+     int[] val = {13, -4, 82, 17}; 
+     int[] twice = new int[4];
 
-        
-        int sum = val[0] + val[1] + val[2] + val[3];
+     System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+     twice[0] = val[0] * 2;
+     twice[1] = val[1] * 2;
+     twice[2] = val[2] * 2;
+     twice[3] = val[3] * 2;
      
-        System.out.println( "Sum of all numbers = " + sum );
+     System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
     }
 }
