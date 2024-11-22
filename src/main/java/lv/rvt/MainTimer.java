762bd27663b7580b5a8b4b1de1f4ae63046c3702
Timurs Kvačovs <166509350+23DP3TKvac@users.noreply.github.com>
2024-11-22 12:06:26 +0000

    One minute

---
+++ src/main/java/lv/rvt/MainTimer.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+public class MainTimer{
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        while (true) {
+            System.out.println(timer);
+            timer.advance();
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+
+    }
+}
+}
+}
