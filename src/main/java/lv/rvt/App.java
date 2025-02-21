f4215caa32712c93472e20a13f1329d6ba0e6af4
Timurs Kvačovs <166509350+23DP3TKvac@users.noreply.github.com>
2025-02-21 10:10:46 +0000

    Warehouse

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,22 +1,12 @@
 package lv.rvt;
 
-public class App {
-
+ public class App {
         public static void main(String[] args) {
-           Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-           Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-                
-           System.out.println(ada);
-           System.out.println(esko);
-                
-           Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-                
-           int i = 0;
-           while (i < 25) {
-                ollie.study();
-                i = i + 1;
-           }
-                
-           System.out.println(ollie);   
+                ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+                juice.addToWarehouse(1000.0);
+                juice.takeFromWarehouse(11.3);
+                System.out.println(juice.getName());
+                juice.addToWarehouse(1.0);
+                System.out.println(juice);       
         }
 }
