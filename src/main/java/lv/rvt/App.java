645402b37fe752c7183f7e69cbf9c89e00878bfb
Timurs Kvačovs <166509350+23DP3TKvac@users.noreply.github.com>
2025-02-21 12:41:33 +0000

    interfeisa krasas

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,20 +2,55 @@ package lv.rvt;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Scanner;
+
 
 public class App {
         public static void main(String[] args) throws Exception {
                 
-                ArrayList<Double> doubleList = new ArrayList<>();
-                doubleList.add(1.0);
-                doubleList.add(2.0);
-                doubleList.add(3.0);
-                doubleList.add(4.0);
-                doubleList.add(5.0);
+                Scanner scanner = new Scanner(System.in);
+                ArrayList<Integer> masivs = new ArrayList<>();
+                System.out.print("\033[0;36m");
+                
 
-                System.out.println(doubleList);
 
-                doubleList.sort(Comparator.reverseOrder());
-                System.out.println(doubleList);
+                while(true) {
+                        
+                        String input = scanner.nextLine();
+                        if (input.equals("0")) {
+                                break;
+                        }
+                        try {
+                                int skaitlis = Integer.parseInt(input);
+                                masivs.add(skaitlis);
+                        } catch (NumberFormatException e) {
+                                System.out.println("Nepareiza ievade!");
+                        }
+                }
+
+
+                masivs.sort(Comparator.naturalOrder());
+
+                System.out.println("Sakartots augošā secībā:\n");
+
+                System.out.printf("+---------+---------+\n");
+                System.out.printf("|  index  |  value  |\n");
+                System.out.printf("+---------+---------+\n");
+                for (int i = 0; i < masivs.size(); i++) {
+                        System.out.printf("|  %5d  |  %6d |\n", i, masivs.get(i));
+                }
+                System.out.printf("+---------+---------+\n");
+
+                masivs.sort(Comparator.reverseOrder());
+
+                System.out.println("Sakartots dilstošā secībā:\n");
+
+                System.out.printf("+---------+---------+\n");
+                System.out.printf("|  index  |  value  |\n");
+                System.out.printf("+---------+---------+\n");
+                for (int i = 0; i < masivs.size(); i++) {
+                        System.out.printf("|  %5d  |  %6d |\n", i, masivs.get(i));
+                }
+                System.out.printf("+---------+---------+\n");
         }
 }
---
+++ src/main/java/lv/rvt/colors.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+public class colors {
+    
+    public static void main(String[] args) {
+        
+    System.out.print("\033[47m");
+    System.out.println("             ");
+
+    System.out.print("\033[44m");
+    System.out.println("             ");
+
+    System.out.print("\033[41m");
+    System.out.println("             ");
+    }
+}
